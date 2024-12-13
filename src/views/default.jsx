286ee5f0e378b57a0422b887e08517a6043539b6
Tauri-23 <airichjaydiawan@gmail.com>
2024-12-13 23:17:17 +0800

    update

--- src/views/default.jsx
+++ src/views/default.jsx
@@ -1,14 +1,31 @@
 import Navigation1 from "../components/navs/navigation1";
 import '../assets/css/home.css';
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { ModalProvider } from "../contexts/ModalContext";
 import ModalManager from "../Managers/ModalManager";
 import Footer1 from "../components/footer/footer1";
 
 
 export default function GuestDefault() {
+    const location = useLocation();
     const [activeNavLink, setActiveNavLink] = useState("Home");
+
+
+
+    /**
+     * Scroll up every change location
+     */
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
+
+
+
+    /**
+     * Render
+     */
     return(
         <ModalProvider>
             <main style={{position: "relative", width: "100%", height: "100%"}}>
